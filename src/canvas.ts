186f5bdea8b5725b379881d13cf735ae333a418c
Jonathan Foss <git@jonathanfoss.co.uk>
2022-03-13 20:37:28 +0000

    Basic message posts

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { memoryUsage } from 'process';
 export class Canvas{
     panel:any;
     initialise(){
@@ -12,34 +15,25 @@ export class Canvas{
         let colors = ['red','green','blue','yellow'];
         let i = -1;
         let currentColour = -1;
-        setInterval(function(){
-            console.log("Sending message");
-            while(i === currentColour){
-                i = Math.floor(Math.random()*colors.length);
-            }
-            currentColour = i;
-            console.log("Sending colour: " + colors[i]);
-            me.panel.webview.postMessage({color: colors[i]});
-        },1000);
-    };
-    getWebviewContent(){
-        return `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
 
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Cat Coding</title>
-        </head>
-        <script>
-            setTimeout(function(){console.log("Ready");},4000);
-            window.addEventListener('message', event => {
-                const message = event.data;
-                document.body.style.backgroundColor = message.color;
-            });
-        </script>
-        <body>
-        </body>
-        </html>`;
+        // this.getWebview().postMessage({color: "white"});
+        // setInterval(function(){
+        //     while(i === currentColour){
+        //         i = Math.floor(Math.random()*colors.length);
+        //     }
+        //     currentColour = i;
+        //     me.panel.webview.postMessage({color: colors[i]});
+        // },1000);
+    };
+
+    getWebview(){
+        return this.panel.webview;
+    };
+
+    getWebviewContent(){
+        let p:string = vscode.extensions.getExtension("EMGroup.vscode-eden-notebook")?.extensionPath || "";
+        if(p === ""){return;} 
+        const filePath: vscode.Uri = vscode.Uri.file(path.join(p,'src','canvas','canvas.html'));
+        return fs.readFileSync(filePath.fsPath,'utf8');
     }
 }
---
+++ src/test.md
@@ -0,0 +1,22 @@
+```js-e
+do lib;
+do lib;
+```
+
+```js-e
+createView("picture3","Canvas2D");
+```
+
+```js-e
+r is Rectangle(10,10,20,30);
+view_picture3_content is [r];
+```
+
+```js-e
+${{CLIEden.edenUI.plugins.Canvas2D.drawPicture("picture3");}}$;
+```
+
+```js-e
+${{console.log(CLIEden.edenUI.plugins.Canvas2D.drawPicture("picture3"));}}$;
+```
+
